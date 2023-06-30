7c0378282f55c414fd14e188f500fbde1f3e8e37
Julian Bilcke <julian.bilcke@huggingface.co>
2023-06-30 15:57:26 +0200

    restart the stream if it get shut down

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,2 +1,9 @@
-echo "starting the FFMPEG stream, to merge video and audio and send it to the RTMP server.."
-ffmpeg -re -i video.pipe -i audio.pipe -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
+#!/bin/bash
+
+echo "starting final stream loop.."
+while true; do
+    sleep 1
+    echo "trying to create the final stream.."
+    ffmpeg -re -i video.pipe -i audio.pipe -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
+    echo "final stream got interrupted, will try again in 1 sec"
+done
