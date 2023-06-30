b8b74f3e9b79ed2e0a433fe53ac60fc17cb8d80a
Julian Bilcke <julian.bilcke@huggingface.co>
2023-06-30 16:03:40 +0200

    trying to debug

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -5,6 +5,7 @@ while true; do
     sleep 1
     for f in $WEBTV_AUDIO_STORAGE_PATH*.mp3
     do
+        echo "playing $f"
         ffmpeg -re -i "$f" -vn -acodec copy -f mp3 -y audio.pipe 2>/dev/null
     done
 done
--- scripts/video.sh
+++ scripts/video.sh
@@ -5,6 +5,7 @@ while true; do
     sleep 1
     for f in $WEBTV_VIDEO_STORAGE_PATH*.mp4
     do
+        echo "playing $f"
         ffmpeg -re -i "$f" -vcodec copy -f mpegts -y video.pipe 2>/dev/null
     done
 done
