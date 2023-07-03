6d85e951da3088cc2ac41eb0e4d081e30be7be03
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-03 15:37:26 +0200

    replace O by 0

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -14,8 +14,8 @@ while true; do
     sleep 1
     ((COUNT++))
     echo "Trying to create the final stream.."
-    ffmpeg -y -nostdin -re -f concat -safe O -i "list_a.txt" -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
-    # ffmpeg -y -nostdin -re -f concat -safe O -i "list_a.txt" -i "audio_list_a.txt" -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
+    ffmpeg -y -nostdin -re -f concat -safe 0 -i "list_a.txt" -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
+    # ffmpeg -y -nostdin -re -f concat -safe 0 -i "list_a.txt" -i "audio_list_a.txt" -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
     
     echo "Final stream got interrupted, will try again in 1 sec"
 done
