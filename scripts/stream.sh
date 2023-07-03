935c12f53fc1a88bb7f2b31ed5da7eb014a4997e
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-03 15:04:38 +0200

    reducing log level

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -4,6 +4,6 @@ echo "Starting final stream loop.."
 while true; do
     sleep 1
     echo "Trying to create the final stream.."
-    ffmpeg -y -nostdin -re -f concat -safe 0 -i "list_a.txt" -i "audio_list_a.txt" -loglevel panic -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
+    ffmpeg -y -nostdin -re -f concat -safe 0 -i "list_a.txt" -i "audio_list_a.txt" -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
     echo "Final stream got interrupted, will try again in 1 sec"
 done
