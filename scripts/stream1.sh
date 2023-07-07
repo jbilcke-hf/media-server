62be26b35bd658ac2dc2163700cf88b69ade6258
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-07 11:11:57 +0200

    new stream

--- scripts/stream1.sh
+++ scripts/stream1.sh
@@ -4,7 +4,5 @@ COUNT=0
 
 echo "Starting FFMPEG live stream for channel 1"
 while true; do
-    # ffmpeg -y -nostdin -re -f concat -safe 0 -i channel_1_video.txt -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/webtv
     ffmpeg -y -nostdin -re -f concat -safe 0 -i channel_1_video.txt -stream_loop -1 -safe 0 -i channel_1_audio.txt -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -shortest -f flv rtmp://localhost/live/webtv
-
 done
