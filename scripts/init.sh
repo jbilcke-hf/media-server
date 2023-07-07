5c9ea84162e26afb899701c9f5ee38dd621e4e93
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-07 11:11:23 +0200

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,7 +5,7 @@ echo "creating the storage folders for channel 1.."
 mkdir -p $WEBTV_VIDEO_STORAGE_PATH_CHANNEL_1
 mkdir -p $WEBTV_AUDIO_STORAGE_PATH_CHANNEL_1
 
-bash download_fresh_music.sh
+bash scripts/download_fresh_music.sh
 
 echo "creating the playlists for channel 1.." 
 echo "ffconcat version 1.0" > channel_1_video.txt
