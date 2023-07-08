40e216b3939a3c7a58b488a1901004298bb3923c
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-08 14:38:44 +0200

    up

--- scripts/download_fresh_music.sh
+++ scripts/download_fresh_music.sh
@@ -1,5 +1,5 @@
 
-echo "Downloading music files.."
+echo "Downloading latest music files.."
 wget https://www.dropbox.com/s/fzxqbu87ul3ctqa/pack1.zip
 unzip -o pack1.zip -d .
 cp *.m4a $WEBTV_AUDIO_STORAGE_PATH_CHANNEL_1
