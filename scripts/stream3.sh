24489efbbf65a69312c9a55c455216a81aafbc51
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-11 20:03:01 +0200

    decimate again

--- scripts/stream3.sh
+++ scripts/stream3.sh
@@ -6,4 +6,5 @@ echo "Starting FFMPEG live stream for channel 3"
 while true; do
     # Note: for now we also use channel 1 for audio!
     ffmpeg -y -nostdin -re -f concat -safe 0 -i channel_3_video.txt -stream_loop -1 -safe 0 -i channel_1_audio.txt -loglevel error -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -shortest -f flv rtmp://localhost/live/smooth
+    sleep 1
 done
